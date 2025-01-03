bec646bb8ba9b4d1ab6408fc589b28c3b66107c1
FathAneesa <faathaneesa@gmail.com>
2025-01-03 15:07:31 +0530

    completed

---
+++ sorting.php
@@ -0,0 +1,14 @@
+<?php
+$name=["aami","aysha","rashi","ani","shifana"];
+$t=$name;
+echo"Displaying using print_r<br>";
+print_r($name);
+echo"<br><br>";
+echo"Sorting using asort() <br>";
+asort($name);
+print_r($name);
+echo"<br><br>";
+echo"Sorting using arsort(() <br>";
+arsort($t);
+print_r($t);
+?>
